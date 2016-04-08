cab27ac815f5de5be73c945ee5be94ecb1c253df
Tomat <victor>
2016-04-08 17:54:22 +0300

    Add load schema and tree view XML file

--- XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/Form1.Designer.cs
+++ XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(134, 238);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -44,18 +46,38 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(31, 28);
+            this.richTextBox1.Location = new System.Drawing.Point(392, 49);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox1.Size = new System.Drawing.Size(302, 183);
+            this.richTextBox1.Size = new System.Drawing.Size(336, 285);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 49);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(357, 285);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(367, 346);
+            this.ClientSize = new System.Drawing.Size(752, 346);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +90,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button2;
 
     }
 }
--- XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/Form1.cs
+++ XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/Form1.cs
@@ -15,20 +15,93 @@ namespace XmlLoadAndGenerationFlatFile
 {
     public partial class Form1 : Form
     {
+        private TreeNode tNode;
         public Form1()
         {
             InitializeComponent();
-            //XmlReader reader = XmlReader.Create("D:/XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/TEST_SPECIFICATION.xml");
         }
         
-
+        /// <summary>
+        /// This function open xml file and treeview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            String path = "D:/XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/TEST_SPECIFICATION.xml";
-            XmlDocument doc = new XmlDocument();
-            doc.Load(path);
-           // XPathNavigator navigator = doc.CreateNavigator();
-            richTextBox1.Text = Xml.GetChild(doc.ChildNodes).ToString();           
-        }   
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open XML Document";
+            dlg.Filter = "XML Files (*.xml)|*.xml";
+            dlg.FileName = Application.StartupPath + "\\..\\..\\example.xml";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    this.Cursor = Cursors.WaitCursor;
+                    XmlDocument xDoc = new XmlDocument();
+                    xDoc.Load(dlg.FileName);
+                    treeView1.Nodes.Clear();
+                    treeView1.Nodes.Add(new TreeNode(xDoc.DocumentElement.Name));                   
+                    tNode = new TreeNode();
+                    tNode = (TreeNode)treeView1.Nodes[0];
+                    addTreeNode(xDoc.DocumentElement, tNode);
+                    //treeView1.ExpandAll();
+                }
+                catch (XmlException xExc) 
+                {
+                    MessageBox.Show(xExc.Message);
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    this.Cursor = Cursors.Default; 
+                }
+            }
+        }
+        
+        /// <summary>
+        /// This function is called recursively until all nodes are loaded
+        /// </summary>
+        /// <param name="xmlNode"></param>
+        /// <param name="treeNode"></param>
+        private void addTreeNode(XmlNode xmlNode, TreeNode treeNode)
+        {
+            XmlNode xNode;
+            TreeNode tNode;
+            XmlNodeList xNodeList;
+
+            if (xmlNode.HasChildNodes)
+            {
+                xNodeList = xmlNode.ChildNodes;
+
+                for (int x = 0; x <= xNodeList.Count - 1; x++)
+                {
+                    xNode = xmlNode.ChildNodes[x];
+                    treeNode.Nodes.Add(new TreeNode(xNode.Name));
+                    tNode = treeNode.Nodes[x];
+                    addTreeNode(xNode, tNode);
+                }
+            }
+            else 
+                treeNode.Text = xmlNode.OuterXml.Trim();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            richTextBox1.Clear();
+                foreach (var item in treeView1.SelectedNode.Nodes)
+                {
+                    richTextBox1.Text += item.ToString().Replace("TreeNode:", "") + "\n";
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog dlg = new SaveFileDialog();
+            //dlg.FileName = this.treeView1.Nodes[0].Text + ".xml";
+            //dlg.Filter = "XML Files (*.xml)|*.xml";			
+        }
     }
 }
