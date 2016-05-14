84a0765a21b29d3fbc76aa0794aa7611b4a539a4
Tomat <victor>
2016-05-14 20:43:30 +0300

    Implement method view attributes element

--- XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/Form1.Designer.cs
+++ XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/Form1.Designer.cs
@@ -28,33 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.treeView1 = new System.Windows.Forms.TreeView();
-            this.button2 = new System.Windows.Forms.Button();
             this.treeView2 = new System.Windows.Forms.TreeView();
             this.nameColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.valueColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.nameColumnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.valueColumnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFlatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "load";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(392, 49);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox1.Size = new System.Drawing.Size(336, 285);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
             // 
             // treeView1
             // 
@@ -62,24 +53,15 @@
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(357, 285);
             this.treeView1.TabIndex = 3;
-            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(108, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Save";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.xmlTreeView_NodeMouseClick);
             // 
             // treeView2
             // 
-            this.treeView2.Location = new System.Drawing.Point(748, 49);
+            this.treeView2.Location = new System.Drawing.Point(392, 49);
             this.treeView2.Name = "treeView2";
             this.treeView2.Size = new System.Drawing.Size(275, 285);
             this.treeView2.TabIndex = 5;
+            this.treeView2.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.xmlTreeView2_NodeMouseClick);
             // 
             // nameColumnHeader
             // 
@@ -91,31 +73,138 @@
             this.valueColumnHeader.Text = "Value";
             this.valueColumnHeader.Width = 130;
             // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameColumnHeader1,
+            this.valueColumnHeader1});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(688, 49);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(322, 285);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // nameColumnHeader1
+            // 
+            this.nameColumnHeader1.Text = "Attribute";
+            this.nameColumnHeader1.Width = 101;
+            // 
+            // valueColumnHeader1
+            // 
+            this.valueColumnHeader1.Text = "Value";
+            this.valueColumnHeader1.Width = 149;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1035, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveXMLToolStripMenuItem,
+            this.saveFlatToolStripMenuItem});
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            // 
+            // saveXMLToolStripMenuItem
+            // 
+            this.saveXMLToolStripMenuItem.Name = "saveXMLToolStripMenuItem";
+            this.saveXMLToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveXMLToolStripMenuItem.Text = "Save XML";
+            // 
+            // saveFlatToolStripMenuItem
+            // 
+            this.saveFlatToolStripMenuItem.Name = "saveFlatToolStripMenuItem";
+            this.saveFlatToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveFlatToolStripMenuItem.Text = "Save Flat";
+            this.saveFlatToolStripMenuItem.Click += new System.EventHandler(this.saveFlatToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1035, 339);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.treeView2);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TreeView treeView2;
         private System.Windows.Forms.ColumnHeader nameColumnHeader;
         private System.Windows.Forms.ColumnHeader valueColumnHeader;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader nameColumnHeader1;
+        private System.Windows.Forms.ColumnHeader valueColumnHeader1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveXMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFlatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
 
     }
 }
--- XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/Form1.cs
+++ XmlLoadAndGenerationFlatFile/XmlLoadAndGenerationFlatFile/Form1.cs
@@ -16,35 +16,201 @@ namespace XmlLoadAndGenerationFlatFile
 {
     public partial class Form1 : Form
     {
-        private TreeNode tNode;
+        XElement root;
         public Form1()
         {
             InitializeComponent();
         }
         
-        /// <summary>
-        /// This function open xml file and treeview
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_Click(object sender, EventArgs e)
+
+        void readerXml(string dlg)
+        {
+            treeView2.Nodes.Clear();
+
+            root = XElement.Load(dlg);
+
+            XDocument doc = XDocument.Load(dlg);
+
+            XmlDocument docs = new XmlDocument();
+
+            docs.Load(dlg);
+
+
+            foreach (XElement el in root.Elements())
+            {
+
+                if (el.Attribute("name") != null && el.Attribute("name").Value == root.FirstAttribute.Value)
+                {
+
+                    treeView2.Nodes.Add(root.FirstAttribute.Value).ForeColor = Color.Green;
+                    AddNodes(root, treeView2.Nodes[0]);
+                }
+                                  
+            }
+        }
+
+        void AddNodes(XElement parentElement, TreeNode parent = null)
+        {
+            Queue<XElement> queue = new Queue<XElement>(parentElement.Elements());
+
+            if (queue.Count > 0)
+            {
+
+                TreeNode child = parent;
+
+                XElement element = queue.Dequeue();
+
+                while (queue.Count > 0)
+                {
+
+                    string value = "";
+
+                    element = (XElement)element.NextNode;
+
+                    value = element.FirstAttribute.Value;
+
+                    if (parent.Text != value)
+                    {
+
+                        if (element != null && !element.HasElements) value = element.FirstAttribute.Value;
+
+                        if (parent == null) { value = element.FirstAttribute.Value; treeView2.Nodes.Add(child = new TreeNode(value)); }
+
+                        else parent.Nodes.Add(child = new TreeNode(value));
+
+                        child.Expand();
+
+
+
+                        AddNodes(element, child);
+
+                    }
+
+                    element = queue.Dequeue();
+
+                }
+
+            }
+
+        }
+
+        private TreeNode XmlNode2TreeNode(XmlNode parentNode)
+        {
+            TreeNode treeNode = new TreeNode();
+
+            foreach (XmlNode childNode in parentNode.ChildNodes)
+            {
+                if (childNode.NodeType == XmlNodeType.Element)
+                {
+                    TreeNode childTreeNode = new TreeNode();
+
+                    if (childNode.ChildNodes.Count > 0)
+                        childTreeNode = XmlNode2TreeNode(childNode);
+
+                    childTreeNode.Text = FormatName(childNode);
+                    GetAttributes(childNode, childTreeNode);
+                    treeNode.Nodes.Add(childTreeNode);
+                }
+            }
+
+            return treeNode;
+        }
+
+        private string FormatName(XmlNode node)
+        {
+            string fullName = "<" + node.Name;
+
+            if (node.InnerText != "")
+                fullName += ">" + node.InnerText + "</" + node.Name + ">";
+            else
+                fullName += ">";
+
+            return fullName;
+        }
+
+        private void GetAttributes(XmlNode node, TreeNode treeNode)
+        {
+            if (node.Attributes != null)
+            {
+                ListViewItem[] attributes = new ListViewItem[node.Attributes.Count];
+
+                for (int i = 0; i < node.Attributes.Count; i++)
+                {
+                    attributes[i] = new ListViewItem(new string[] { node.Attributes[i].Name, node.Attributes[i].Value });
+                }
+
+                treeNode.Tag = attributes;
+            }
+        }
+        private void xmlTreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            listView1.Items.Clear();
+
+            if (e.Node.Tag != null)
+            {
+                listView1.Items.AddRange((ListViewItem[])e.Node.Tag);
+            }
+        }
+
+        private void xmlTreeView2_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            listView1.Items.Clear();
+            
+            foreach (XElement el in root.Elements())
+            {
+                if (el.HasAttributes && el.FirstAttribute.Value == e.Node.Text)
+                {
+                    if (el.Attributes() != null)
+                    {
+                        ListViewItem[] attributes = new ListViewItem[el.Attributes().Count()];
+                        int i = 0;
+                        foreach (var item in el.Attributes())
+                        {
+                            attributes[i] = new ListViewItem(new string[] { item.Name.ToString(), item.Value });
+                            i++;
+                        }
+
+                        listView1.Items.AddRange(attributes);
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog dlg = new SaveFileDialog();
+            //dlg.FileName = this.treeView1.Nodes[0].Text + ".xml";
+            //dlg.Filter = "XML Files (*.xml)|*.xml";			
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
             dlg.Title = "Open XML Document";
             dlg.Filter = "XML Files (*.xml)|*.xml";
-            dlg.FileName = Application.StartupPath + "\\..\\..\\example.xml";
+            dlg.FileName = Application.StartupPath + "\\..\\..\\TEST_SPECIFICATION.xml";
             if (dlg.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
-                    this.Cursor = Cursors.WaitCursor;
-                    XmlDocument xDoc = new XmlDocument();
-                    xDoc.Load(dlg.FileName);
+
                     treeView1.Nodes.Clear();
-                    treeView1.Nodes.Add(new TreeNode(xDoc.DocumentElement.Name));
-                    tNode = new TreeNode();
-                    tNode = (TreeNode)treeView1.Nodes[0];
-                    addTreeNode(xDoc.DocumentElement, tNode);
+
+                    XmlDocument document = new XmlDocument();
+                    document.Load(dlg.FileName);   //load the XMl file
+
+                    XmlNodeList nodeList = document.DocumentElement.ChildNodes;
+
+                    //loop through the top nodes
+                    foreach (XmlNode node in document)
+                    {
+                        TreeNode treeNode = XmlNode2TreeNode(node);
+                        GetAttributes(node, treeNode);
+                        treeNode.Text = FormatName(node);
+                        treeView1.Nodes.Add(treeNode);
+                    }
+
                     treeView1.ExpandAll();
                 }
                 catch (XmlException xExc)
@@ -60,112 +226,18 @@ namespace XmlLoadAndGenerationFlatFile
                     this.Cursor = Cursors.Default;
                 }
             }
-
-           // XmlTextReader reader = new XmlTextReader(dlg.FileName);
-            readerXml(dlg.FileName);
-            
-}
-
-        void readerXml(string dlg)
-        {
-            treeView2.Nodes.Clear();
-            XElement root = XElement.Load(dlg);
-            XDocument doc = XDocument.Load(dlg);
-            XmlDocument docs = new XmlDocument();
-            docs.Load(dlg);
-           
-            foreach (XElement el in root.Elements())
-            {
-                if (el.Attribute("name") != null && el.Attribute("name").Value == root.FirstAttribute.Value)
-                {
-                    treeView2.Nodes.Add(root.FirstAttribute.Value).ForeColor = Color.Green;
-                }
-
-               // reader(el);       
-            }
-            //foreach (XElement el in doc.Root.Elements())
-            //{
-            //        //Console.WriteLine("{0}", el.Name.LocalName);
-                             
-            //    Console.WriteLine("  Elements:");
-           
-            //    foreach (XElement element in el.Elements())
-            //    {
-            //        Console.WriteLine("    {0}", element.FirstAttribute.Value);
-            //        //foreach (XAttribute attr in el.Attributes())
-            //        //Console.WriteLine("    {0}", attr.Value);
-            //    }
-            //}
-            AddNodes(XElement.Load(dlg));
+            if (dlg.FileName != null)
+                readerXml(dlg.FileName);
         }
 
-        void AddNodes(XElement parentElement, TreeNode parent = null)
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Queue<XElement> queue = new Queue<XElement>(parentElement.Elements());
-            while (queue.Count > 1)
-            {
-                TreeNode child = parent;
-                XElement element = queue.Dequeue();
-                if (!element.HasElements)
-                {
-                    string value = "";
-                    //if(element.FirstAttribute.Value!=null)
-                    //value =""+ element.FirstAttribute.Value; 
-                    element = (XElement)element.NextNode;
-                    if (element != null && !element.HasElements) value = element.FirstAttribute.Value;
-
-                    if (parent == null) { value = element.FirstAttribute.Value; treeView2.Nodes.Add(child = new TreeNode(value)); }
-                    else parent.Nodes.Add(child = new TreeNode(value));
-                    child.Expand();
-                    element = queue.Dequeue();
-                }
-                AddNodes(element, child);
-            }
+            this.Hide();
         }
 
-//----------------------------------------------------------------------------------------------------        
-     
-        /// <summary>
-        /// This function is called recursively until all nodes are loaded
-        /// </summary>
-        /// <param name="xmlNode"></param>
-        /// <param name="treeNode"></param>
-        private void addTreeNode(XmlNode xmlNode, TreeNode treeNode)
+        private void saveFlatToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            XmlNode xNode;
-            TreeNode tNode;
-            XmlNodeList xNodeList;
 
-            if (xmlNode.HasChildNodes)
-            {
-                xNodeList = xmlNode.ChildNodes;
-
-                for (int x = 0; x <= xNodeList.Count - 1; x++)
-                {
-                    xNode = xmlNode.ChildNodes[x];
-                    treeNode.Nodes.Add(new TreeNode(xNode.Name));
-                    tNode = treeNode.Nodes[x];
-                    addTreeNode(xNode, tNode);
-                }
-            }
-            else 
-                treeNode.Text = xmlNode.OuterXml.Trim();
-        }
-
-        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            richTextBox1.Clear();
-                foreach (var item in treeView1.SelectedNode.Nodes)
-                {
-                    richTextBox1.Text += item.ToString().Replace("TreeNode:", "") + "\n";
-                }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            //SaveFileDialog dlg = new SaveFileDialog();
-            //dlg.FileName = this.treeView1.Nodes[0].Text + ".xml";
-            //dlg.Filter = "XML Files (*.xml)|*.xml";			
         }
     }
 }
